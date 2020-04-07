d3d6710d60b39a7efc3ded511983ddad74afe3f1
shaojintian <sjt@hnu.edu.cn>
2020-04-07 11:49:13 +0800

    singleton

---
+++ src/singleton/lazy/SecuritySingleton.java
@@ -0,0 +1,56 @@
+package singleton.lazy;
+
+import java.io.Serializable;
+
+public class SecuritySingleton {
+
+    private SecuritySingleton(){}
+
+    private  static SecuritySingleton singleton = null;
+
+    /**
+     * 1.
+     * 同步
+     */
+    /**
+     * public synchronized static SecuritySingleton getInstance(){
+
+        if(singleton == null){
+            singleton = new SecuritySingleton();
+        }
+
+        return singleton;
+    }
+     */
+
+    /**
+     * 2.double check
+     * @return
+
+    private volatile static SecuritySingleton singleton =  null;
+
+    public static  SecuritySingleton getInstance(){
+        if (singleton==null){
+            synchronized (SecuritySingleton.class){
+                if(singleton==null){
+                    singleton = new SecuritySingleton();
+                }
+            }
+        }
+
+        return singleton;
+    }
+    */
+
+    /**
+     * 3.静态内部类 最好，不用同步损耗性能，实现线程安全
+     */
+    private static class LazyHolder{
+        private static final SecuritySingleton  INSTANCE = new SecuritySingleton();
+    }
+
+    public static SecuritySingleton getInstance(){
+        return LazyHolder.INSTANCE;
+    }
+
+}
